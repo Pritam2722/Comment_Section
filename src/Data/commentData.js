44c11bbc31b5e3e238692497dde5480aff97ef3a
Coder's Den 07 <138252202+codersden07@users.noreply.github.com>
2023-07-31 01:41:52 +0530

    Add files via upload

---
+++ src/Data/commentData.js
@@ -0,0 +1,34 @@
+export const commentData = {
+  id: "1",
+  text: "Hello world! How are you?",
+  replies: [
+    {
+      id: "2",
+      text: "Hey, I am fine, wau?",
+      replies: [
+        {
+          id: "3",
+          text: "Hello, I am food, How are you?",
+          replies: [
+            {
+              id: "4",
+              text: "Life sucks man!",
+              replies: [
+                {
+                  id: "5",
+                  text: "Haha, I can relate :)",
+                  replies: [],
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+    {
+      id: "6",
+      text: "Hey everyone!",
+      replies: [],
+    },
+  ],
+};
